cfd2686a12a237ea63c3c907512ba0a8f364c8c8
baek min gyu <msmk908@naver.com>
2024-08-14 12:12:50 +0900

    2024-08-14
    업로드 화면 데이터셋 설정 완료

--- src/renderer/UploadComponent.jsx
+++ src/renderer/UploadComponent.jsx
@@ -1,49 +1,99 @@
-// src/UploadComponent.jsx
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
-import 'tailwindcss/tailwind.css'; // 테일윈드 css 적용
+import 'tailwindcss/tailwind.css';
 
 function UploadComponent() {
   const [files, setFiles] = useState([]); // 첨부한 파일 저장
   const [titles, setTitles] = useState({}); // 파일 제목 저장
   const [encodings, setEncodings] = useState({}); // 파일 인코딩 설정 저장
+  const [resolutions, setResolutions] = useState({}); // 원본 해상도 저장
   const [uploadStatus, setUploadStatus] = useState(''); // 업로드 상태
   const fileInputRef = useRef(null); // 파일첨부 기능
 
-  const handleFileChange = (e) => {
+  // 첨부파일 추가 메서드
+  const handleFileChange = async (e) => {
     const newFiles = Array.from(e.target.files);
+    const newResolutions = await getFileInfo(newFiles, files.length); // 파일 인덱스를 전달
 
     setFiles((prevFiles) => {
       const updatedFiles = [...prevFiles, ...newFiles];
+      setResolutions((prevResolutions) => ({
+        ...prevResolutions,
+        ...newResolutions,
+      }));
 
-      // 새 파일에 대해 기본 인코딩 설정 추가
-      newFiles.forEach((_, index) => {
-        setEncodings((prevEncodings) => ({
-          ...prevEncodings,
-          [prevFiles.length + index]: [{ format: 'jpg', resolution: '1080p' }],
-        }));
-      });
+      const newEncodings = newFiles.reduce((acc, file, index) => {
+        const fileIndex = prevFiles.length + index;
+        const isImage = file.type.startsWith('image/');
+        acc[fileIndex] = [
+          { format: isImage ? 'jpg' : 'mp4', resolution: '1080p' },
+        ];
+        return acc;
+      }, {});
+
+      setEncodings((prevEncodings) => ({
+        ...prevEncodings,
+        ...newEncodings,
+      }));
 
       return updatedFiles;
     });
   };
 
-  const handleDrop = (e) => {
+  // 드래그 드롭 메서드
+  const handleDrop = async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
     const newFiles = Array.from(e.dataTransfer.files);
+    const newResolutions = await getFileInfo(newFiles, files.length); // 파일 인덱스를 전달
 
     setFiles((prevFiles) => {
       const updatedFiles = [...prevFiles, ...newFiles];
+      setResolutions((prevResolutions) => ({
+        ...prevResolutions,
+        ...newResolutions,
+      }));
 
-      // 새 파일에 대해 기본 인코딩 설정 추가
-      newFiles.forEach((_, index) => {
-        setEncodings((prevEncodings) => ({
-          ...prevEncodings,
-          [prevFiles.length + index]: [{ format: 'jpg', resolution: '1080p' }],
-        }));
+      const newEncodings = newFiles.reduce((acc, file, index) => {
+        const fileIndex = prevFiles.length + index;
+        const isImage = file.type.startsWith('image/');
+        acc[fileIndex] = [
+          { format: isImage ? 'jpg' : 'mp4', resolution: '1080p' },
+        ];
+        return acc;
+      }, {});
+
+      setEncodings((prevEncodings) => ({
+        ...prevEncodings,
+        ...newEncodings,
+      }));
+
+      return updatedFiles;
+    });
+  };
+
+  // 파일 삭제 핸들러
+  const handleFileDelete = (index) => {
+    setFiles((prevFiles) => {
+      // 삭제할 파일을 제외한 새로운 파일 목록 생성
+      const updatedFiles = prevFiles.filter((_, i) => i !== index);
+
+      // 삭제된 파일의 인덱스에 해당하는 상태만 제거
+      const updatedEncodings = {};
+      const updatedResolutions = {};
+      const updatedTitles = {};
+
+      updatedFiles.forEach((_, i) => {
+        // 기존의 인덱스 맵을 유지
+        updatedEncodings[i] = encodings[i >= index ? i + 1 : i] || [];
+        updatedResolutions[i] = resolutions[i >= index ? i + 1 : i] || {};
+        updatedTitles[i] = titles[i >= index ? i + 1 : i] || '';
       });
+
+      setEncodings(updatedEncodings);
+      setResolutions(updatedResolutions);
+      setTitles(updatedTitles);
 
       return updatedFiles;
     });
@@ -53,32 +103,37 @@ function UploadComponent() {
     e.preventDefault();
   };
 
+  // 제목 설정
   const handleTitleChange = (index, e) => {
     const newTitles = { ...titles, [index]: e.target.value };
     setTitles(newTitles);
   };
 
+  // 포맷 설정
   const handleFormatChange = (fileIndex, encodingIndex, e) => {
     const newEncodings = { ...encodings };
     newEncodings[fileIndex][encodingIndex].format = e.target.value;
     setEncodings(newEncodings);
   };
 
+  // 해상도 설정
   const handleResolutionChange = (fileIndex, encodingIndex, e) => {
     const newEncodings = { ...encodings };
     newEncodings[fileIndex][encodingIndex].resolution = e.target.value;
     setEncodings(newEncodings);
   };
 
+  // 포맷 해상도 추가
   const addEncoding = (fileIndex) => {
     const newEncodings = { ...encodings };
     newEncodings[fileIndex] = [
       ...newEncodings[fileIndex],
-      { format: 'jpg', resolution: '1080p' },
+      { format: encodings[fileIndex][0].format, resolution: '1080p' }, // 동일한 포맷으로 추가
     ];
     setEncodings(newEncodings);
   };
 
+  // 포맷 해상도 제거
   const removeEncoding = (fileIndex, encodingIndex) => {
     const newEncodings = { ...encodings };
     if (newEncodings[fileIndex].length > 1) {
@@ -95,8 +150,73 @@ function UploadComponent() {
     e.stopPropagation();
   };
 
-  const handleUpload = (e) => {
+  const handleUpload = () => {
+    console.log('Files:', files);
+    console.log('Titles:', titles);
+    console.log('Encodings:', encodings);
+    console.log('Resolutions:', resolutions);
     // 여기에 파일 업로드 로직을 추가하세요
+  };
+
+  // 파일의 정보를 가져오는 함수
+  const getFileInfo = (files, startIndex) => {
+    return Promise.all(
+      files.map((file, index) => {
+        return new Promise((resolve) => {
+          const isImage = file.type.startsWith('image/');
+          const isVideo = file.type.startsWith('video/');
+          const url = URL.createObjectURL(file);
+
+          if (isImage) {
+            const img = new Image();
+            img.onload = () => {
+              resolve({
+                [startIndex + index]: {
+                  width: img.width,
+                  height: img.height,
+                  playtime: null,
+                },
+              });
+              URL.revokeObjectURL(img.src);
+            };
+            img.src = url;
+          } else if (isVideo) {
+            const video = document.createElement('video');
+            video.onloadedmetadata = () => {
+              resolve({
+                [startIndex + index]: {
+                  width: video.videoWidth,
+                  height: video.videoHeight,
+                  playtime: video.duration,
+                },
+              });
+              URL.revokeObjectURL(video.src);
+            };
+            video.onerror = () => {
+              resolve({
+                [startIndex + index]: {
+                  width: null,
+                  height: null,
+                  playtime: null,
+                },
+              });
+              URL.revokeObjectURL(video.src);
+            };
+            video.src = url;
+          } else {
+            resolve({
+              [startIndex + index]: {
+                width: null,
+                height: null,
+                playtime: null,
+              },
+            });
+          }
+        });
+      }),
+    ).then((resolutions) => {
+      return resolutions.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+    });
   };
 
   return (
@@ -115,83 +235,107 @@ function UploadComponent() {
         )}
 
         <div className="p-10">
-          {files.map((file, fileIndex) => (
-            <div
-              key={fileIndex}
-              className="border p-4 mt-4 mb-4 rounded-md shadow-sm flex"
-              onClick={handleChildClick}
-            >
-              <div className="w-1/2 pr-4">
-                <img
-                  src={URL.createObjectURL(file)}
-                  alt={file.name}
-                  className="w-full h-auto object-cover"
-                />
+          {files.map((file, fileIndex) => {
+            const isImage = file.type.startsWith('image/');
+            return (
+              <div
+                key={fileIndex}
+                className="relative border p-4 mt-4 mb-4 rounded-md shadow-sm flex"
+                onClick={handleChildClick}
+              >
+                <button
+                  onClick={() => handleFileDelete(fileIndex)}
+                  className="absolute top-2 right-2 text-red-500 hover:text-red-700"
+                >
+                  &times;
+                </button>
+                <div className="w-1/2 pr-4">
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={file.name}
+                    className="w-full h-auto object-cover"
+                  />
+                </div>
+
+                <div className="w-1/2 pr-4">
+                  <label className="block mb-2 font-bold">{file.name}</label>
+                  <input
+                    type="text"
+                    placeholder="제목을 입력해주세요."
+                    className="p-2 border rounded-md w-full mb-4"
+                    value={titles[fileIndex] || ''}
+                    onChange={(e) => handleTitleChange(fileIndex, e)}
+                  />
+
+                  {encodings[fileIndex]?.map((encoding, encodingIndex) => (
+                    <div key={encodingIndex} className="flex space-x-4 mb-4">
+                      <div className="w-1/4">
+                        <select
+                          className="p-2 border rounded-md w-full"
+                          value={encoding.format}
+                          onChange={(e) =>
+                            handleFormatChange(fileIndex, encodingIndex, e)
+                          }
+                        >
+                          {isImage ? (
+                            <>
+                              <option value="jpg">JPG</option>
+                              <option value="png">PNG</option>
+                            </>
+                          ) : (
+                            <>
+                              <option value="mp4">MP4</option>
+                              <option value="mov">MOV</option>
+                              <option value="avi">AVI</option>
+                            </>
+                          )}
+                        </select>
+                      </div>
+
+                      <div className="w-1/4">
+                        <select
+                          className="p-2 border rounded-md w-full"
+                          value={encoding.resolution}
+                          onChange={(e) =>
+                            handleResolutionChange(fileIndex, encodingIndex, e)
+                          }
+                        >
+                          <option value="720p">720p</option>
+                          <option value="1080p">1080p</option>
+                          <option value="4k">4K</option>
+                        </select>
+                      </div>
+
+                      {encodingIndex === encodings[fileIndex].length - 1 && (
+                        <>
+                          <div className="w-1/4">
+                            <button
+                              className="w-full p-2 bg-blue-500 text-white rounded"
+                              onClick={() => addEncoding(fileIndex)}
+                            >
+                              +
+                            </button>
+                          </div>
+
+                          <div className="w-1/4">
+                            <button
+                              className="w-full p-2 bg-red-500 text-white rounded"
+                              onClick={() =>
+                                removeEncoding(fileIndex, encodingIndex)
+                              }
+                              disabled={encodings[fileIndex].length <= 1}
+                            >
+                              -
+                            </button>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
-
-              <div className="w-1/2 pr-4">
-                <label className="block mb-2 font-bold">{file.name}</label>
-                <input
-                  type="text"
-                  placeholder="제목을 입력해주세요."
-                  className="p-2 border rounded-md w-full mb-4"
-                  value={titles[fileIndex] || ''}
-                  onChange={(e) => handleTitleChange(fileIndex, e)}
-                />
-
-                {encodings[fileIndex].map((encoding, encodingIndex) => (
-                  <div key={encodingIndex} className="flex space-x-4 mb-4">
-                    <div className="w-1/4">
-                      <select
-                        className="p-2 border rounded-md w-full"
-                        value={encoding.format}
-                        onChange={(e) =>
-                          handleFormatChange(fileIndex, encodingIndex, e)
-                        }
-                      >
-                        <option value="jpg">JPG</option>
-                        <option value="png">PNG</option>
-                        <option value="gif">GIF</option>
-                      </select>
-                    </div>
-
-                    <div className="w-1/4">
-                      <select
-                        className="p-2 border rounded-md w-full"
-                        value={encoding.resolution}
-                        onChange={(e) =>
-                          handleResolutionChange(fileIndex, encodingIndex, e)
-                        }
-                      >
-                        <option value="720p">720p</option>
-                        <option value="1080p">1080p</option>
-                        <option value="4k">4K</option>
-                      </select>
-                    </div>
-
-                    <div className="w-1/4">
-                      <button
-                        className="w-full p-2 bg-blue-500 text-white rounded"
-                        onClick={() => addEncoding(fileIndex)}
-                      >
-                        +
-                      </button>
-                    </div>
-
-                    <div className="w-1/4">
-                      <button
-                        className="w-full p-2 bg-red-500 text-white rounded"
-                        onClick={() => removeEncoding(fileIndex, encodingIndex)}
-                        disabled={encodings[fileIndex].length <= 1}
-                      >
-                        -
-                      </button>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         <input
